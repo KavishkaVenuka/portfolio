4c931b0895e4137908d86b9561d56780a3eae534
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-03-02 22:02:45 +0530

    Add the source code files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+import AboutMe from './components/AboutMe'
+import Projects from './components/Projects'
+
+export default function App() {
+  return (
+    <div>
+      <Hero />
+      <AboutMe />
+      <Projects />
+    </div>
+  )
+}
