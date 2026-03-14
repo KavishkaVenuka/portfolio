8da642ab27f9e521c44b42276d178928d4097f2a
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-03-14 10:55:33 +0530

    Add the skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import AboutMe from './components/AboutMe'
 import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Hero />
       <AboutMe />
       <Projects />
+      <Skills />
     </div>
   )
 }
